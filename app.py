b2c79af56514dfb82e89d595a1a3a0f9d7e20db7
Ishan Joshi <ishajosh@amazon.com>
2021-06-18 22:21:49 +1000

    fix: concise starter code

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
 from aws_cdk import core
-from cdk_config.deployment_env import aws_account_info
 
-# from pipeline.demo_api import LambdaApiDev, LambdaApiProd
-
-from pipeline.pipeline_stack import PipelineStack
+from pipeline.demo_api import LambdaApiDev, LambdaApiProd
 
 app = core.App()
 
-# dev_stack = LambdaApiDev(app, "LambdaApiDev", env=aws_account_info)
-# prod_stack = LambdaApiProd(app, "LambdaApiProd", env=aws_account_info)
-
-PipelineStack(app, 'PipelineStack', env=aws_account_info)
+dev_stack = LambdaApiDev(app, "LambdaApiDev")
+prod_stack = LambdaApiProd(app, "LambdaApiProd")
 
 app.synth()
