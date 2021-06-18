f2423ddea9b789f5d5c135a90f5fc50a25f4cd0b
Ishan Joshi <ishajosh@amazon.com>
2021-06-18 22:48:35 +1000

    fix: solution reformatting

--- app.py
+++ app.py
@@ -2,14 +2,9 @@
 from aws_cdk import core
 from cdk_config.deployment_env import aws_account_info
 
-# from pipeline.demo_api import LambdaApiDev, LambdaApiProd
-
 from pipeline.pipeline_stack import PipelineStack
 
 app = core.App()
-
-# dev_stack = LambdaApiDev(app, "LambdaApiDev", env=aws_account_info)
-# prod_stack = LambdaApiProd(app, "LambdaApiProd", env=aws_account_info)
 
 PipelineStack(app, 'PipelineStack', env=aws_account_info)
 
